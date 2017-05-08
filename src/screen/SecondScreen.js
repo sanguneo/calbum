11380be4e25dad6ddc3485a8fca8ff2c6317e8cb
나상권 <sknah@imicorp.co.kr>
2017-05-08 18:32:58 +0900

    - Alert added

--- src/screen/SecondScreen.js
+++ src/screen/SecondScreen.js
@@ -40,14 +40,14 @@ export default class SecondScreen extends Component {
         }
         if (event.id === 'edit') {
             Alert.alert('알림!!!', '두번째 EDIT');
-            this.props.navigator.resetTo({
+            /*this.props.navigator.resetTo({
                 screen: 'calbum.SecondScreen', // unique ID registered with Navigation.registerScreen
                 title: undefined, // navigation bar title of the pushed screen (optional)
                 passProps: {}, // simple serializable object that will pass as props to the pushed screen (optional)
                 animated: true, // does the push have transition animation or does it happen immediately (optional)
                 navigatorStyle: {}, // override the navigator style for the pushed screen (optional)
                 navigatorButtons: {} // override the nav buttons for the pushed screen (optional)
-            });
+            });*/
         }
     }
 
