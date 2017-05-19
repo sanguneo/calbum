6f18fe58bcadf42b46dd5aa573816fd8de3b5fdd
sanguneo <sknah0319@gmail.com>
2017-05-19 18:32:06 +0900

    - sqlite

--- src/service/calbumdb_svc.js
+++ src/service/calbumdb_svc.js
@@ -4,10 +4,10 @@
 
 const SQLite = require('react-native-sqlite-storage');
 export default class dbSvc {
-    constructor = () => {
+    constructor() {
         this.setDB(SQLite.openDatabase({name: 'consultAlbum.db', location: 'Library'}));
     }
-    initialDB = () => {
+    initialDB() {
         this.db.transaction((tx) => {
             tx.executeSql('CREATE TABLE "ca_user" (`idx` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,`unuque_key` TEXT NOT NULL UNIQUE,`reg_date` TEXT NOT NULL,`user_id` TEXT NOT NULL);', []);
             tx.executeSql('CREATE TABLE "ca_tags" (`idx` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,`photo_key` TEXT,`album_key` TEXT,`user_key` TEXT,`unique_key` TEXT NOT NULL UNIQUE);', []);
@@ -19,14 +19,14 @@ export default class dbSvc {
             tx.executeSql('CREATE INDEX `album_idx` ON `ca_album` (`idx` ASC ,`unique_key` );', []);
         });
     }
-    setDB = (db) => {
+    setDB(db) {
         this.db = db;
         return this.db;
     }
-    getDB = () => {
+    getDB() {
         return this.db;
     }
-    regUSER = (uniqkey, regdate, userid) => {
+    regUSER(uniqkey, regdate, userid) {
         this.db.transaction((tx) => {
             tx.executeSql("INSERT INTO `ca_user` VALUES (null,'${uniqkey}','${regdate}','${userid}');", []);
         });
